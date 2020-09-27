7de792c83012b5a9f6972361242cb8e0f55d9ccf
Adil Jamal <adiljamal252@gmail.com>
2020-09-27 17:00:43 +0500

    added login and register page

--- src/App.js
+++ src/App.js
@@ -10,33 +10,35 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='App'>
-      <div className='loading'>
-        <div className='loader'></div>
-      </div>
-      <a id='scroll-up'>
-        <i className='fa fa-angle-up'></i>
-      </a>
+    <Router>
+      <div className='App'>
+        <div className='loading'>
+          <div className='loader'></div>
+        </div>
+        <a id='scroll-up'>
+          <i className='fa fa-angle-up'></i>
+        </a>
 
-      <div className='theme-settings' id='switcher'>
-        {' '}
-        <span className='theme-click'>
-          <i className='fa fa-cog fa-spin' aria-hidden='true'></i>
-        </span>{' '}
-        <span
-          className='theme-color theme-default theme-active'
-          data-color='green'
-        ></span>{' '}
-        <span className='theme-color theme-blue' data-color='blue'></span>{' '}
-        <span className='theme-color theme-red' data-color='red'></span>{' '}
-        <span className='theme-color theme-violet' data-color='violet'></span>
-        <p>(Or) Your favorite color</p>
+        <div className='theme-settings' id='switcher'>
+          {' '}
+          <span className='theme-click'>
+            <i className='fa fa-cog fa-spin' aria-hidden='true'></i>
+          </span>{' '}
+          <span
+            className='theme-color theme-default theme-active'
+            data-color='green'
+          ></span>{' '}
+          <span className='theme-color theme-blue' data-color='blue'></span>{' '}
+          <span className='theme-color theme-red' data-color='red'></span>{' '}
+          <span className='theme-color theme-violet' data-color='violet'></span>
+          <p>(Or) Your favorite color</p>
+        </div>
+        <TopBar />
+        <Nav />
+        <KonnectSlider />
+        <Footer />
       </div>
-      <TopBar />
-      <Nav />
-      <KonnectSlider />
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
